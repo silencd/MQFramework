3b42d10e989d928d4d34037ba7b39e3c6c72a495
developerd@163.com <developerd@163.com>
2016-07-15 11:47:11 +0800

    fix

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 return [
     'DB_TYPE' => 'mysql',
     'DB_HOST' => '127.0.0.1',
-    'DB_NAME' => 'wahaha',
+    'DB_NAME' => 'demo',
     'DB_USERNAME' => 'root',
     'DB_PASSWD' => 'root',
     'DB_TABLE_PREFIX' => '',
